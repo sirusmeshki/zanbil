18dcba73ef0b2fbb21a213787cc06f9ceca40bb9
AmirHossein <sirusmeshki@gmail.com>
2024-01-23 21:21:40 +0330

    add: temporary home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import SignIn from '@/dump/sign-in'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Form } from '@/components/ui/form'
+import Image from 'next/image'
+
 export default function Home() {
-    return <></>
+    return (
+        <main className='flex h-screen flex-col p-2'>
+            <h1 className='my-10 text-center text-4xl font-bold'>Home</h1>
+        </main>
+    )
 }
