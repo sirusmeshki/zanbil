8935f5410358f28cb53248e53e3aa90d2b43e40f
AmirHossein <sirusmeshki@gmail.com>
2024-01-23 21:30:22 +0330

    fix: home page unnecessary imports remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { ModeToggle } from '@/components/mode-toggle'
-import SignIn from '@/dump/sign-in'
-import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
-import { Form } from '@/components/ui/form'
-import Image from 'next/image'
-
 export default function Home() {
     return (
         <main className='flex h-screen flex-col p-2'>
