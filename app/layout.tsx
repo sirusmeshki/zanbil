ab11587a038ae820e57cd7824db927b5bff33272
AmirHossein <sirusmeshki@gmail.com>
2024-01-24 07:25:59 +0330

    style: mode toggle animation activated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,7 @@ export default function RootLayout({
                 <ThemeProvider
                     attribute='class'
                     defaultTheme='system'
-                    enableSystem
-                    disableTransitionOnChange>
+                    enableSystem>
                     <Header />
                     {children}
                 </ThemeProvider>
