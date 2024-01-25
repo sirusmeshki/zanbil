37fd9c14b6b7d2eefa77eb6fdc2e861bccb172c8
AmirHossein <sirusmeshki@gmail.com>
2024-01-25 06:01:49 +0330

    add: layout folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config = {
         './components/**/*.{ts,tsx}',
         './app/**/*.{ts,tsx}',
         './src/**/*.{ts,tsx}',
+        './layout/**/*.{ts,tsx}',
     ],
     prefix: '',
     theme: {
