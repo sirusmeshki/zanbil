36bb30f5c222c9de507c63ad9e3dbb2bc454f165
AmirHossein <sirusmeshki@gmail.com>
2024-01-27 20:53:25 +0330

    add: dashboard condition render on role

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { auth } from '@/auth'
+
 const DashboardPage = async () => {
-    return <div>Dashboard Page</div>
+    const session = await auth()
+
+    if (session?.user?.role === 'ADMIN') return <h1>Admin Dashboard</h1>
+    if (session?.user?.role === 'USER') return <h1>User Dashboard</h1>
+
+    return <h2>hey</h2>
 }
 
 export default DashboardPage
