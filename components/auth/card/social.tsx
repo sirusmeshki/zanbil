e3d088779c1d7088321236e813a41f445b4acbd0
AmirHossein <sirusmeshki@gmail.com>
2024-01-27 20:52:55 +0330

    add: google sign in on click action

--- components/auth/card/social.tsx
+++ components/auth/card/social.tsx
@@ -1,15 +1,20 @@
 import Image from 'next/image'
+
 import { Button } from '@/components/ui/button'
+import { googleSignIn } from '@/actions/google'
 
 const Social = () => {
     return (
-        <Button className='w-full' variant='outline'>
+        <Button
+            className='w-full'
+            variant='outline'
+            onClick={() => googleSignIn()}>
             <Image
+                className='mr-2'
                 src='/google.svg'
                 alt='google logo'
                 width={18}
                 height={18}
-                className='mr-2'
             />
             Login with Google
         </Button>
